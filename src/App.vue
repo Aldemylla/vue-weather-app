573a9c37a354b560f2fb0f27e82b14106ed66c4a
Aldemylla <aldemylla@gmail.com>
2022-09-28 12:27:23 -0300

    feat: HTML struture finished

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <p>teste</p>
-  </div>
+  <router-view />
 </template>
 
 <script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section>
+      <label for="city-search">
+        <input type="search" id="city-search" />
+      </label>
+      <div>
+        <h2>Natal, BR</h2>
+        <div>
+          <h1>31ºc</h1>
+        </div>
+        <h2>Feels like 30ºc</h2>
+        <h2>Clear sky</h2>
+      </div>
+      <div>
+        <div>
+          <Icon icon="bx:wind" />
+          <h3>6.58</h3>
+        </div>
+        <div>
+          <Icon icon="bx:droplet" />
+          <h3>32%</h3>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { Icon } from '@iconify/vue2';
+
+export default {
+  name: 'ViewHome',
+
+  components: {
+    Icon,
+  },
+};
+</script>
+
+<style></style>
